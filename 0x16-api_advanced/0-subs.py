ae8e4bf220c0ab54e3c0332b54b33c5c8cc801d8
254jack <taixonj@gmail.com>
2024-01-17 18:32:17 +0300

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
 Function that queries the Reddit API and returns the number of subscribers
-(not active users, total subscribers) for a given subReddit.
+for a given subReddit.
 """
 
 import requests
